70af7a21fa023d2fad4e80d30fe223259883af2b
Bobby Read <54912588+Bobby-Read@users.noreply.github.com>
2019-12-12 15:36:39 -0500

    Added alien piece and a better player

---
+++ Model/Pieces/Alien.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Pieces
+{
+    //The Alien can move like the rook, but only 3 spaces and it can jump pieces of the same color
+
+    public class Alien : BasePiece
+    {
+        protected override char Char => '☊';
+
+        private static Vector[] DirectionsForward = new Vector[] {
+            new Vector(0, 1),
+            new Vector(0, 2),
+            new Vector(0, 3),
+        };
+
+        private static Vector[] DirectionsBack = new Vector[] {
+            new Vector(0, -1),
+            new Vector(0, -2),
+            new Vector(0, -3),
+        };
+
+        private static Vector[] DirectionsRight = new Vector[]
+            {
+            new Vector(1, 0),
+            new Vector(2, 0),
+            new Vector(3, 0),
+            };
+
+        private static Vector[] DirectionsLeft = new Vector[]
+        {
+            new Vector(-1, 0),
+            new Vector(-2, 0),
+            new Vector(-3, 0),
+        };
+
+        public override BasePiece[][,] GetMoves(BasePiece[,] board)
+        {
+            //New list to store potential moves
+            var boards = new List<BasePiece[,]>();
+
+            foreach (var directions in new[] { DirectionsForward, DirectionsBack, DirectionsLeft, DirectionsRight })
+            {
+                foreach (var direction in directions)
+                {
+                    var landed = Location + direction;
+                    if (IsOnBoard(landed))
+                    {
+                        if (board[landed.X, landed.Y] == null)
+                        {
+                            boards.Add(CloneBoardAndMove<Alien>(board, landed));
+                        }
+                        //if there's a piece to capture add the move then break
+                        else if (board[landed.X, landed.Y].Color != Color)
+                        {
+                            boards.Add(CloneBoardAndMove<Alien>(board, landed));
+                            break;
+                        }
+                    }
+                    else break;
+                }
+            }
+            return boards.ToArray();
+
+        }
+    }
+}
---
+++ Model/Players/BetterPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class BetterPlayer : BasePlayer
+    {
+
+        //iterates through all moves, makes a move if it results in a capture, else makes the first move in the options array
+        public override int ChooseMove(BasePiece[][,] options)
+        {
+            int bestIndex = 0;
+            for(int i = 0;  i < options.Length; i++)
+            {
+                if(PieceCaptured(options, options[i]))
+                {
+                    return i;
+                }
+            }
+            return bestIndex;
+        }
+
+        //checks if there are fewer pieces on the board in this move than the first possible option
+        virtual protected bool PieceCaptured(BasePiece[][,] options, BasePiece[,] move)
+        {
+            int firstMovePieces = 0;
+            int currMovePieces = 0;
+            foreach(BasePiece p in options[0])
+            {
+                if (p != null)
+                {
+                    firstMovePieces++;
+                }
+            }
+
+            foreach(BasePiece p in move)
+            {
+                if(p != null)
+                {
+                    currMovePieces++;
+                }
+            }
+
+            if(currMovePieces < firstMovePieces)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
